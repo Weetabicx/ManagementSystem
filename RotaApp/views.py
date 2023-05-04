e6750e96c6ad5c232f9d0befefd4cd8530e7aba9
Usmaan Wahab <usmaanwahab11@outlook.com>
2023-05-04 21:36:32 +0100

    feat: views/processsing for requests

--- RotaApp/views.py
+++ RotaApp/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.template import loader
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import User
 
-# Create your views here.
+from datetime import datetime, timedelta
+
+def user_login(request):
+
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+      login(request,user)
+      return redirect('rota')
+    form = LoginForm(request.POST)
+    return render(request, 'login.html', {'form': form})
+
+  else:
+    form = LoginForm(request.POST)
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def availability(request):
+    if request.method == "POST":
+      avail = request.POST.getlist('availability')
+      user = request.user
+      user.availability  = avail
+      user.save()
+      return redirect('rota')
+      
+    today = datetime.now().weekday()
+    monday = datetime.now() - timedelta(days=today)
+
+    days = {}
+    for i in range(0, 7):
+      days[i] = (monday + timedelta(days=i)).strftime('%A %d/%m/%Y')
+
+
+    return render(request, 'availability.html',
+                  {'days': days.values(),
+                  'weekdays': range(0,7)})
+
+@login_required
+def rota(request):
+    today = datetime.now().weekday()
+    monday = datetime.now() - timedelta(days=today)
+
+    days = {}
+    for i in range(0, 7):
+      days[i] = (monday + timedelta(days=i)).strftime('%A %d/%m/%Y')
+
+
+    return render(request, 'rota.html',
+                  {'days': days.values(),
+                  'weekdays': range(0,7)})
